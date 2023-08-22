2c961969b52083b7b4b0d8942b86803dae782022
Mikhail Rudakas <uncleru2004@rambler.ru>
2023-08-22 18:12:14 +0300

    !!

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
-//import css from './nav.css';
+import css from './nav.module.css';
 
 export default function Nav() {
-    return <nav>
+    return <nav className={css.nav}>
         <ul>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/about">About</Link></li>
